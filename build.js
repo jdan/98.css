9338217743681a556f8473b4a4de2fd885bb02d2
Jordan Scales <scalesjordan@gmail.com>
2020-04-21 13:41:54 -0400

    helper code for rendering examples

--- build.js
+++ build.js
@@ -10,12 +10,29 @@ function getCurrentId() {
   return id;
 }
 
+function example(code) {
+  const magicBrackets = /\[\[(.*)\]\]/g;
+  const inline = code.replace(magicBrackets, "$1");
+  const escaped = code
+    .replace(magicBrackets, "")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;");
+
+  return `<div class="example">
+    ${inline}
+    <details>
+      <summary>Show code</summary>
+      <pre><code>${escaped}</code></pre>
+    </details>
+  </div>`;
+}
+
 function buildDocs() {
   const template = fs.readFileSync("./docs/index.html.ejs", "utf-8");
 
   fs.writeFileSync(
     "./docs/index.html",
-    ejs.render(template, { getNewId, getCurrentId })
+    ejs.render(template, { getNewId, getCurrentId, example })
   );
 }
 
